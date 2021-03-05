3266e98ff6c26e5b25c2f543b53461976fd1da4f
Niclas <Nic@annas-mbp.mynet>
2021-03-05 23:09:50 +0100

    added funcs for adding new images and deleting images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import { v4 as uuid} from 'uuid'
 
 const Gallery = ()=> {
 
@@ -11,31 +12,37 @@ const Gallery = ()=> {
   
     const [content, setContent] = useState([
         {
-          
+          id: uuid(),
           img: "/img/01.jpg",
           descr: 'Some txt goes her'
         },
         {
+          id: uuid(),
           img: '/img/02.jpg',
           descr: 'Some more txt goes her'
         },
         {
+          id: uuid(),
           img: '/img/03.jpg',
           descr: 'Some more txt goes her'
         },
         {
+          id: uuid(),  
           img: '/img/04.jpg',
           descr: 'Some more txt goes her'
         },
         {
+          id: uuid(),
           img: '/img/05.jpg',
           descr: 'Some more txt goes her'
         },
         {
+          id: uuid(),
           img: '/img/06.jpg',
           descr: 'Some more txt goes here for img6'
         },
         {
+          id: uuid(),
           img: '/img/07.jpg',
           descr: 'Some more txt goes her'
         }
@@ -44,13 +51,23 @@ const Gallery = ()=> {
     const handleSubmit = (e)=>{
         e.preventDefault()
         if(newImage && descr) {
-        const db = [...content, {img: newImage,
-        descr: descr}]
+        const db = [...content, {
+          id: uuid(),  
+          img: newImage,
+          descr: descr}]
 
         console.log('our db', db)
         setContent(db)
         }
     }
+
+    const handleDelete = (clicked)=> {
+        const filterArray = content.filter(itm => itm.id !== clicked)
+        console.log(filterArray)
+        setContent(filterArray)
+    }
+
+    console.log(content)
 
     return(
         <div className="app">
@@ -62,9 +79,10 @@ const Gallery = ()=> {
         
         <p>{newImage}</p>
         <p>{descr}</p>
-            
+            {/* mapping over our 'db' here */}
             {content.map(itm=> (
-            <div className="card"key={Math.random()}>
+            <div className="card"key={itm.id}>
+              <span className="delete__itm"onClick={()=>handleDelete(itm.id)}>X</span>  
               <p><img src={itm.img} alt="#" onClick={()=> {
                 setProj(itm.img)
                 setModal(prev => !prev)}}/></p>
